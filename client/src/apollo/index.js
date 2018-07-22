90da15aa4b5d2a86f24b8852e6dd9e611e640df0
benjaminadk <benjaminadk@gmail.com>
2018-07-21 17:46:22 -0700

    refactor apollo uri

--- client/src/apollo/index.js
+++ client/src/apollo/index.js
@@ -3,7 +3,12 @@ import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloLink, concat } from 'apollo-link'
 
-const httpLink = new HttpLink({ uri: 'http://localhost:5001/graphql' })
+const httpLink = new HttpLink({
+  uri:
+    process.env.NODE_ENV === 'production'
+      ? 'https://annes-admin.herokuapp.com/graphql'
+      : 'http://localhost:5001/graphql'
+})
 const authMiddleware = new ApolloLink((operation, forward) => {
   operation.setContext(({ headers = {} }) => ({
     headers: {
